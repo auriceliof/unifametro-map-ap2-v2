ccb30fcfb73cdda3032a2f1a81e78d028e8acba6
auriceliof <auricelio.suporte@hotmail.com>
2023-05-15 17:38:57 -0300

    Segmentação: main, barra, bola

---
+++ bola.py
@@ -0,0 +1,55 @@
+import random
+
+count = 0
+
+def game_over():
+    print("Game Over")
+
+class Bola:
+    def __init__(self, canvas, Barra, color):
+        self.canvas = canvas
+        self.Barra = Barra
+        self.id = canvas.create_oval(0, 0, 15, 15, fill=color)
+        self.canvas.move(self.id, 245, 200)
+
+        starts_x = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts_x)
+
+        self.x = starts_x[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+
+        if pos[1] <= 0:
+            self.y = 3
+
+        if pos[3] >= self.canvas_height:
+            self.y = -3
+
+        if pos[0] <= 0:
+            self.x = 3
+
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+
+        self.Barra_pos = self.canvas.coords(self.Barra.id)
+
+        if pos[2] >= self.Barra_pos[0] and pos[0] <= self.Barra_pos[2]:
+            if pos[3] >= self.Barra_pos[1] and pos[3] <= self.Barra_pos[3]:
+                self.y = -3
+                global count
+                count += 1
+
+                #score()
+
+        if pos[3] <= self.canvas_height:
+            self.canvas.after(10, self.draw)
+        else:
+            game_over()
+
+            global lost
+            lost = True
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from tkinter import *
-import random
 import time
 from PIL import Image, ImageTk
 
 from barra import *
+from bola import *
+
+Bola = Bola
+Barra = Barra
 
 def set_level(event):
     global level, length
@@ -39,8 +42,8 @@ def start_game(event=None):
 def score():
     canvas.itemconfig(score_now, text="Você acertou " + str(count) + "x")
 
-def game_over():
-    canvas.itemconfig(game, text="Game over!")
+#def game_over():
+#    canvas.itemconfig(game, text="Game over!")
 
 root = Tk()
 root.title("Ping Pong")
@@ -64,56 +67,5 @@ level_entry.pack()
 submit_button = Button(root, text="Enviar", font=("Arial", 20), command=lambda: set_level(None))
 submit_button.pack()
 submit_button.bind("<Return>", set_level)
-
-count = 0
-
-class Bola:
-    def __init__(self, canvas, Barra, color):
-        self.canvas = canvas
-        self.Barra = Barra
-        self.id = canvas.create_oval(0, 0, 15, 15, fill=color)
-        self.canvas.move(self.id, 245, 200)
-
-        starts_x = [-3, -2, -1, 1, 2, 3]
-        random.shuffle(starts_x)
-
-        self.x = starts_x[0]
-        self.y = -3
-        self.canvas_height = self.canvas.winfo_height()
-        self.canvas_width = self.canvas.winfo_width()
-
-    def draw(self):
-        self.canvas.move(self.id, self.x, self.y)
-        pos = self.canvas.coords(self.id)
-
-        if pos[1] <= 0:
-            self.y = 3
-
-        if pos[3] >= self.canvas_height:
-            self.y = -3
-
-        if pos[0] <= 0:
-            self.x = 3
-
-        if pos[2] >= self.canvas_width:
-            self.x = -3
-
-        self.Barra_pos = self.canvas.coords(self.Barra.id)
-
-        if pos[2] >= self.Barra_pos[0] and pos[0] <= self.Barra_pos[2]:
-            if pos[3] >= self.Barra_pos[1] and pos[3] <= self.Barra_pos[3]:
-                self.y = -3
-                global count
-                count += 1
-
-                score()
-
-        if pos[3] <= self.canvas_height:
-            self.canvas.after(10, self.draw)
-        else:
-            game_over()
-
-            global lost
-            lost = True
 
 root.mainloop()
